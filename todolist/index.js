e7b52562940750471210960547f728c7624a192f
howie_wang <1121486527@qq.com>
2021-01-20 23:38:16 +0800

    update

--- todolist/index.js
+++ todolist/index.js
@@ -1,8 +1,24 @@
 ;
 (function () {
-  let finishedDom = document.querySelector("main finished-container")//已完成
+  const localStorage = window.localStorage
 
-  console.log(finishedDom);
+  class Task {
+    constructor({ name, message, starttime, deadline }) {
+      this.id = new Date().getTime()
+      this.name = name
+      this.message = message
+      this.starttime = starttime
+      this.deadline = deadline
+    }
+  }
+  let task = new Task({
+    name: '第一任务',
+    message: '赶快搞完',
+    starttime: '2021/01/22'
+  })
+  console.log(task);
+  localStorage.setItem(task.id, JSON.stringify(task))
+  // let finishedDom = document.querySelector("main finished-container")//已完成
 
 
 })();
